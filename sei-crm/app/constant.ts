1e1e4234dba93670c04ab4fa5ff8e9b5304fd00d
sei-omm <sei.ommdigitalsoluction@gmail.com>
2024-12-03 19:03:02 +0530

    update config

--- sei-crm/app/constant.ts
+++ sei-crm/app/constant.ts
@@ -1,3 +1,3 @@
-export const BASE_API= 'http://localhost:8080/api/v1';
+// export const BASE_API= 'http://localhost:8080/api/v1';
 // export const BASE_API= 'http://192.168.0.192:8080/api/v1';
-// export const BASE_API = "https://main-api-murex.vercel.app/api/v1";
+export const BASE_API = "https://main-api-murex.vercel.app/api/v1";
