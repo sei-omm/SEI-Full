67a5bb23449315c80592f33fcaddf8e5a57b94b0
sei-omm <sei.ommdigitalsoluction@gmail.com>
2024-12-03 15:28:48 +0530

    update postgress

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,19 +1,19 @@
 import { Client, Pool, PoolConfig } from "pg";
 
-export const dbConfig = {
-  user: "postgres",
-  host: "localhost",
-  database: "sei_db",
-  password: "123456",
-  port: 5432,
-};
-
-// export const dbConfig: PoolConfig = {
-//   connectionString: "postgres://default:Sbn2tCg5dilv@ep-soft-darkness-a45ok4wz-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+// export const dbConfig = {
+//   user: "postgres",
+//   host: "localhost",
+//   database: "sei_db",
+//   password: "123456",
+//   port: 5432,
 // };
+
+export const dbConfig: PoolConfig = {
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 export const pool = new Pool(dbConfig);
 export const client = new Client(dbConfig);
