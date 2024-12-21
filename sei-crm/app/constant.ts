30a181366b4bb93abe9a487f5d57601dcdeb6270
sei-omm <sei.ommdigitalsoluction@gmail.com>
2024-12-21 13:23:15 +0530

    fix some env related problem

--- sei-crm/app/constant.ts
+++ sei-crm/app/constant.ts
@@ -1,6 +1,4 @@
-export const BASE_API = "http://localhost:8080/api/v1";
-// export const BASE_API= 'http://192.168.0.192:8080/api/v1';
-// export const BASE_API = "https://main-api-murex.vercel.app/api/v1";
+export const BASE_API = process.env.NEXT_PUBLIC_BASE_API;
 
 export const inventoryCatList = [
   { category_id: 1, category_name: "Consumable" },
--- sei-frontend/app/constant.ts
+++ sei-frontend/app/constant.ts
@@ -1,8 +1,1 @@
-export const BASE_API = "http://localhost:8080/api/v1";
-export const BASE_API_UPLOAD = "http://localhost:8081/api/v1";
-
-// export const BASE_API = process.env.BASE_API;
-// export const BASE_API_UPLOAD = process.env.BASE_API_UPLOAD;
-
-// export const BASE_API = "https://main-api-murex.vercel.app/api/v1";
-// export const BASE_API_UPLOAD = "https://api-upload-nine.vercel.app/api/v1";
+export const BASE_API = process.env.NEXT_PUBLIC_BASE_API;
