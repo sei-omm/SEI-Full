cdfb209cc9d1e802b1bcef0d94b4f5a2a0a7123e
sei-omm <sei.ommdigitalsoluction@gmail.com>
2024-12-21 12:58:49 +0530

    update postgress config

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,19 +1,19 @@
 import { Client, Pool, PoolConfig } from "pg";
 
-export const dbConfig : PoolConfig = {
-  user: "postgres",
-  host: "localhost",
-  database: "sei_db",
-  password: "123456",
-  port: 5432,
-};
-
-// export const dbConfig: PoolConfig = {
-//   connectionString: process.env.POSTGRES_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+// export const dbConfig : PoolConfig = {
+//   user: "postgres",
+//   host: "localhost",
+//   database: "sei_db",
+//   password: "123456",
+//   port: 5432,
 // };
+
+export const dbConfig: PoolConfig = {
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 export const pool = new Pool(dbConfig);
 export const client = new Client(dbConfig);
