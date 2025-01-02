dbc427efa426df981f6df20f297b1004a22287ac
sei-omm <sei.ommdigitalsoluction@gmail.com>
2025-01-02 11:37:35 +0530

    update cookie options

--- sei-frontend/app/actions/cookies.ts
+++ sei-frontend/app/actions/cookies.ts
@@ -6,14 +6,19 @@ import { TLoginSuccess } from "../type";
 export const setCookie = async (key: string, value: string) => {
   const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
 
-  (await cookies()).set(key, value, { expires, httpOnly: true });
+  (await cookies()).set(key, value, {
+    expires,
+    httpOnly: true,
+    secure : true,
+    sameSite: "none"
+  });
 };
 
 export const getAuthTokenServer = async () => {
   const cookieStore = await cookies();
-  const loginInfo = cookieStore.get("login-info")?.value
+  const loginInfo = cookieStore.get("login-info")?.value;
   let token = "";
-  if(loginInfo) {
+  if (loginInfo) {
     token = (JSON.parse(loginInfo) as TLoginSuccess).token;
   }
   return {
