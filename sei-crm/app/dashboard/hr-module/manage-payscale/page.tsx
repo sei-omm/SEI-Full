c9672277bb90655899f96d59366d3e3e761a941b
sei-omm <sei.ommdigitalsoluction@gmail.com>
2025-01-20 19:27:56 +0530

    fix some eslint bugs

--- sei-crm/app/dashboard/hr-module/manage-payscale/page.tsx
+++ sei-crm/app/dashboard/hr-module/manage-payscale/page.tsx
@@ -13,23 +13,9 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { MdAdd } from "react-icons/md";
 import { useQuery } from "react-query";
 
-// type TTable = {
-//   heads: string[];
-//   body: (string | null)[][];
-// };
-
-async function getPayscales() {
-  return (await axios.get(`${BASE_API}/hr/payscale?item_type=Both`)).data;
-}
-
 export default function ManagePayscale() {
   const [labelInput, setLabelInput] = useState<number[]>([]);
   const [yearsInput, setYearsInput] = useState<number[]>([]);
-
-  // const [tableDatas, setTableDatas] = useState<TTable>({
-  //   heads: ["Payscale Label", "Payscale Year", "Action"],
-  //   body: [],
-  // });
 
   const whichDeleteBtnId = useRef<number>(0);
   const { isLoading: isAdding, mutate: add } = useDoMutation();
@@ -42,7 +28,8 @@ export default function ManagePayscale() {
     refetch,
   } = useQuery<ISuccess<TPayscaleBoth>>({
     queryKey: "get-payscale",
-    queryFn: getPayscales,
+    queryFn: async () =>
+      (await axios.get(`${BASE_API}/hr/payscale?item_type=Both`)).data,
   });
 
   function handleFormSubmit(
@@ -75,28 +62,34 @@ export default function ManagePayscale() {
     preStates.push(
       preStates.length === 0 ? 1 : preStates[preStates.length - 1] + 1
     );
-    if (type === "Payscale Label") return setLabelInput(preStates);
-    setYearsInput(preStates);
+    if (type === "Payscale Label") {
+      setLabelInput(preStates);
+    } else {
+      setYearsInput(preStates);
+    }
   }
 
   function handleDeleteRow(index: number, type: "Year" | "Payscale Label") {
     const newInputs =
       type === "Payscale Label" ? [...labelInput] : [...yearsInput];
     newInputs.splice(index, 1);
-    type === "Payscale Label"
-      ? setLabelInput(newInputs)
-      : setYearsInput(newInputs);
-  }
 
+    if (type === "Payscale Label") {
+      setLabelInput(newInputs);
+    } else {
+      setYearsInput(newInputs);
+    }
+  }
   function handleDeleteDb(item_id: number) {
-    if (!confirm("Are you sure you want to delete this item?")) return;
-    deleteData({
-      apiPath: `/hr/payscale/${item_id}`,
-      method: "delete",
-      onSuccess() {
-        refetch();
-      },
-    });
+    if (confirm("Are you sure you want to delete this item?")) {
+      deleteData({
+        apiPath: `/hr/payscale/${item_id}`,
+        method: "delete",
+        onSuccess() {
+          refetch();
+        },
+      });
+    }
   }
 
   return (
@@ -119,162 +112,166 @@ export default function ManagePayscale() {
           Add Payscale Years
         </Button>
       </div>
+      <>
+        {labelInput.length === 0 ? null : (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault(); // Prevent default form submission behavior
+              const formData = new FormData(e.target as HTMLFormElement);
+              handleFormSubmit(formData, "Payscale Label");
+            }}
+            className="mt-4 space-y-5"
+          >
+            <ul className="flex items-start gap-5 flex-wrap">
+              {labelInput.map((item, index) => (
+                <li
+                  key={item}
+                  className="flex items-end gap-5 basis-60 flex-grow *:flex-grow"
+                >
+                  <Input
+                    name="payscale_label"
+                    required
+                    label="Payscale Label *"
+                    placeholder="Type Here.."
+                  />
+                  <AiOutlineDelete
+                    onClick={() => handleDeleteRow(index, "Payscale Label")}
+                    className="mb-4 cursor-pointer active:scale-90 flex-grow-0"
+                  />
+                  {/* <div className="flex items-center gap-4 *:flex-grow flex-grow"> */}
 
-      {labelInput.length === 0 ? null : (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault(); // Prevent default form submission behavior
-            const formData = new FormData(e.target as HTMLFormElement);
-            handleFormSubmit(formData, "Payscale Label");
-          }}
-          className="mt-4 space-y-5"
-        >
-          <ul className="flex items-start gap-5 flex-wrap">
-            {labelInput.map((item, index) => (
-              <li
-                key={item}
-                className="flex items-end gap-5 basis-60 flex-grow *:flex-grow"
-              >
-                <Input
-                  name="payscale_label"
-                  required
-                  label="Payscale Label *"
-                  placeholder="Type Here.."
-                />
-                <AiOutlineDelete
-                  onClick={() => handleDeleteRow(index, "Payscale Label")}
-                  className="mb-4 cursor-pointer active:scale-90 flex-grow-0"
-                />
-                {/* <div className="flex items-center gap-4 *:flex-grow flex-grow"> */}
-
-                {/* <Input
+                  {/* <Input
                     name="payscale_year"
                     required
                     label="Payscale Year *"
                     placeholder="Type Here.."
                     type="number"
                   /> */}
-                {/* </div> */}
-              </li>
-            ))}
-          </ul>
+                  {/* </div> */}
+                </li>
+              ))}
+            </ul>
 
-          <div className="flex items-center justify-end">
-            <Button loading={isAdding} disabled={isAdding}>
-              Save Info
-            </Button>
-          </div>
-        </form>
-      )}
+            <div className="flex items-center justify-end">
+              <Button loading={isAdding} disabled={isAdding}>
+                Save Info
+              </Button>
+            </div>
+          </form>
+        )}
+      </>
 
+      <>
+        {yearsInput.length === 0 ? null : (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault(); // Prevent default form submission behavior
+              const formData = new FormData(e.target as HTMLFormElement);
+              handleFormSubmit(formData, "Year");
+            }}
+            className="mt-4 space-y-5"
+          >
+            <ul className="flex items-start gap-5 flex-wrap">
+              {yearsInput.map((item, index) => (
+                <li
+                  key={item}
+                  className="flex items-end gap-5 basis-60 flex-grow *:flex-grow"
+                >
+                  <Input
+                    name="payscale_year"
+                    required
+                    label="Payscale Year *"
+                    placeholder="Type Here.."
+                    type="number"
+                  />
+                  <AiOutlineDelete
+                    onClick={() => handleDeleteRow(index, "Year")}
+                    className="mb-4 cursor-pointer active:scale-90 flex-grow-0"
+                  />
+                </li>
+              ))}
+            </ul>
 
-      {yearsInput.length === 0 ? null : (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault(); // Prevent default form submission behavior
-            const formData = new FormData(e.target as HTMLFormElement);
-            handleFormSubmit(formData, "Year");
-          }}
-          className="mt-4 space-y-5"
+            <div className="flex items-center justify-end">
+              <Button loading={isAdding} disabled={isAdding}>
+                Save Info
+              </Button>
+            </div>
+          </form>
+        )}
+      </>
+      <>
+        <HandleSuspence
+          isLoading={isFetching}
+          error={error}
+          dataLength={payscale?.data.label.length}
         >
-          <ul className="flex items-start gap-5 flex-wrap">
-            {yearsInput.map((item, index) => (
-              <li
-                key={item}
-                className="flex items-end gap-5 basis-60 flex-grow *:flex-grow"
-              >
-                <Input
-                  name="payscale_year"
-                  required
-                  label="Payscale Year *"
-                  placeholder="Type Here.."
-                  type="number"
-                />
-                <AiOutlineDelete
-                  onClick={() => handleDeleteRow(index, "Year")}
-                  className="mb-4 cursor-pointer active:scale-90 flex-grow-0"
-                />
-              </li>
-            ))}
-          </ul>
-
-          <div className="flex items-center justify-end">
-            <Button loading={isAdding} disabled={isAdding}>
-              Save Info
-            </Button>
+          <div className="space-y-5">
+            <h2 className="font-semibold text-xl text-gray-500">
+              Payscale Labels
+            </h2>
+            <ul className="w-full grid grid-cols-4 gap-6">
+              {payscale?.data.label.map((info) => (
+                <li
+                  key={info.item_id}
+                  className="card-shdow p-5 rounded-xl border border-gray-200"
+                >
+                  <h2 className="font-semibold">{info.item_value}</h2>
+                  <div className="w-full flex items-center justify-end gap-4 *:cursor-pointer">
+                    {isDeleting && whichDeleteBtnId.current === info.item_id ? (
+                      <Spinner size="18px" />
+                    ) : (
+                      <AiOutlineDelete
+                        onClick={() => {
+                          whichDeleteBtnId.current = info.item_id;
+                          handleDeleteDb(info.item_id);
+                        }}
+                        className="active:scale-90"
+                      />
+                    )}
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-        </form>
-      )}
-
-      <HandleSuspence
-        isLoading={isFetching}
-        error={error}
-        dataLength={payscale?.data.label.length}
-      >
-        <div className="space-y-5">
-          <h2 className="font-semibold text-xl text-gray-500">
-            Payscale Labels
-          </h2>
-          <ul className="w-full grid grid-cols-4 gap-6">
-            {payscale?.data.label.map((info) => (
-              <li
-                key={info.item_id}
-                className="card-shdow p-5 rounded-xl border border-gray-200"
-              >
-                <h2 className="font-semibold">{info.item_value}</h2>
-                <div className="w-full flex items-center justify-end gap-4 *:cursor-pointer">
-                  {isDeleting && whichDeleteBtnId.current === info.item_id ? (
-                    <Spinner size="18px" />
-                  ) : (
-                    <AiOutlineDelete
-                      onClick={() => {
-                        whichDeleteBtnId.current = info.item_id;
-                        handleDeleteDb(info.item_id);
-                      }}
-                      className="active:scale-90"
-                    />
-                  )}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </HandleSuspence>
-
-      <HandleSuspence
-        isLoading={isFetching}
-        error={error}
-        dataLength={payscale?.data.year.length}
-      >
-        <div className="space-y-5">
-          <h2 className="font-semibold text-xl text-gray-500">
-            Payscale Years
-          </h2>
-          <ul className="w-full grid grid-cols-4 gap-6">
-            {payscale?.data.year.map((info) => (
-              <li
-                key={info.item_id}
-                className="card-shdow p-5 rounded-xl border border-gray-200"
-              >
-                <h2 className="font-semibold">{info.item_value}</h2>
-                <div className="w-full flex items-center justify-end gap-4 *:cursor-pointer">
-                  {isDeleting && whichDeleteBtnId.current === info.item_id ? (
-                    <Spinner size="18px" />
-                  ) : (
-                    <AiOutlineDelete
-                      onClick={() => {
-                        whichDeleteBtnId.current = info.item_id;
-                        handleDeleteDb(info.item_id);
-                      }}
-                      className="active:scale-90"
-                    />
-                  )}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </HandleSuspence>
+        </HandleSuspence>
+      </>
+      <>
+        <HandleSuspence
+          isLoading={isFetching}
+          error={error}
+          dataLength={payscale?.data.year.length}
+        >
+          <div className="space-y-5">
+            <h2 className="font-semibold text-xl text-gray-500">
+              Payscale Years
+            </h2>
+            <ul className="w-full grid grid-cols-4 gap-6">
+              {payscale?.data.year.map((info) => (
+                <li
+                  key={info.item_id}
+                  className="card-shdow p-5 rounded-xl border border-gray-200"
+                >
+                  <h2 className="font-semibold">{info.item_value}</h2>
+                  <div className="w-full flex items-center justify-end gap-4 *:cursor-pointer">
+                    {isDeleting && whichDeleteBtnId.current === info.item_id ? (
+                      <Spinner size="18px" />
+                    ) : (
+                      <AiOutlineDelete
+                        onClick={() => {
+                          whichDeleteBtnId.current = info.item_id;
+                          handleDeleteDb(info.item_id);
+                        }}
+                        className="active:scale-90"
+                      />
+                    )}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </HandleSuspence>
+      </>
     </div>
   );
 }
