a9f9e384be3979fbbf6f45aaa3d786f0cc2c552a
sei-omm <sei.ommdigitalsoluction@gmail.com>
2025-02-21 22:50:58 +0530

    Add User Selected Campus To Cookie

--- sei-frontend/app/components/Dialogs/SelectOurCenter.tsx
+++ sei-frontend/app/components/Dialogs/SelectOurCenter.tsx
@@ -5,9 +5,14 @@ import DialogBody from "./DialogBody";
 import Button from "../Button";
 import { useDispatch } from "react-redux";
 import { setDialog } from "@/app/redux/slice/dialog.slice";
+import { useTransition } from 'react';
+import { setInfo } from "@/app/utils/saveInfo";
 
 export default function SelectOurCenter() {
   const dispatch = useDispatch();
+  const [isSavingKolkata, setKolkata] = useTransition();
+  const [isSavingFaridabad, setFaridabad] = useTransition();
+  
   return (
     <DialogBody preventToClose = {true}>
       <div className="space-y-5 *:block">
@@ -16,9 +21,14 @@ export default function SelectOurCenter() {
         </span>
         <Link href={"/our-courses/kolkata"}>
           <Button
+            disable = {isSavingKolkata}
+            loading = {isSavingKolkata}
             onClick={() => {
-              dispatch(setDialog({ type: "CLOSE", dialogKey: "" }));
-              localStorage.setItem("user-selected-institute", "Kolkata");
+              setKolkata(async () => {
+                await setInfo("user-selected-institute", "Kolkata");
+                dispatch(setDialog({ type: "CLOSE", dialogKey: "" }));
+                localStorage.setItem("user-selected-institute", "Kolkata");
+              })
             }}
             className="!bg-[#E9B858] !text-black !border-black !w-full hover:!bg-white"
           >
@@ -27,9 +37,14 @@ export default function SelectOurCenter() {
         </Link>
         <Link href={"/our-courses/faridabad"}>
           <Button
+            disable = {isSavingFaridabad}
+            loading = {isSavingFaridabad}
             onClick={() => {
-              dispatch(setDialog({ type: "CLOSE", dialogKey: "" }));
-              localStorage.setItem("user-selected-institute", "Faridabad");
+              setFaridabad(async () => {
+                await setInfo("user-selected-institute", "Faridabad");
+                dispatch(setDialog({ type: "CLOSE", dialogKey: "" }));
+                localStorage.setItem("user-selected-institute", "Faridabad");
+              })
             }}
             className="!bg-[#E9B858] !text-black !border-black !w-full hover:!bg-white"
           >
