27b912c3f14e881e6d05c01e014bb3648fdaad4b
sei-omm <sei.ommdigitalsoluction@gmail.com>
2025-04-04 11:21:16 +0530

    fix the problem of time table subject not selection

--- sei-crm/components/Course/CellTimeTable.tsx
+++ sei-crm/components/Course/CellTimeTable.tsx
@@ -24,7 +24,7 @@ interface IProps {
   subjectInfo: string[];
   time_table_data: TTimeTable[] | undefined;
   virtual_table: TVirtualTable | null;
-  from_draft : boolean;
+  from_draft: boolean;
 }
 
 type TFacultyInfo = {
@@ -56,7 +56,7 @@ export default function CellTimeTable({
   setDuplicateCell,
   duplicateCell,
   virtual_table,
-  from_draft
+  from_draft,
 }: IProps) {
   const [currentFaculties, setCurrentFaculties] = useState<TFacultyInfo[]>(
     () => {
@@ -86,7 +86,9 @@ export default function CellTimeTable({
 
     if (!virtual_table) return alert("No Row Data Found");
 
-    const virtual_table_array = Object.entries(virtual_table || {}).filter(item => item[1].fac !== null && item[1].fac.faculty_id !== -1);
+    const virtual_table_array = Object.entries(virtual_table || {}).filter(
+      (item) => item[1].fac !== null && item[1].fac.faculty_id !== -1
+    );
 
     // not opimize O(n2)
     // now check duplicity
@@ -98,22 +100,19 @@ export default function CellTimeTable({
       const currentFac = value[1].fac?.faculty_id;
       const currentFacCol = value[0].split(":")[1];
 
-      virtual_table_array.forEach(item => {
-
+      virtual_table_array.forEach((item) => {
         // if (item[1].fac === null) return;
         // if (item[1].fac.faculty_id === -1) return;
         if (item[0] === value[0]) return; // if same cell dont check
 
-        if(currentFac === item[1].fac?.faculty_id) {
+        if (currentFac === item[1].fac?.faculty_id) {
           // now check if both are same col
           if (currentFacCol === item[0].split(":")[1]) {
             duplicateCell[item[0]] = true;
             duplicateCell[value[0]] = true;
           }
-
         }
-      })
-
+      });
     });
 
     setDuplicateCell(duplicateCell);
@@ -128,33 +127,36 @@ export default function CellTimeTable({
     virtual_table[`${row}:${col}`] = {
       fac: currentSubFacInfo?.[0] || null,
       subject: subject_name,
-      course_name : time_table_data?.[row].course_name || ""
+      course_name: time_table_data?.[row].course_name || "",
     };
 
     setCurrentFaculties(currentSubFacInfo || []);
     setSingleFacInfo(currentSubFacInfo?.[0] || null);
+    checkDuplicateFac();
   };
 
   const handleFacChange = (fac_id: number) => {
-    const singleFacInfo =
+    const singleFac =
       currentFaculties.find((fac) => fac.faculty_id === fac_id) || null;
-    setSingleFacInfo(singleFacInfo);
+    setSingleFacInfo(singleFac);
     if (!virtual_table) return alert("No Row Data Found");
 
+    const vtSubject = virtual_table[`${row}:${col}`]?.subject;
+
     virtual_table[`${row}:${col}`] = {
-      fac: singleFacInfo,
-      subject: value || "Off Period",
-      course_name : time_table_data?.[row].course_name || ""
+      fac: singleFac,
+      subject: vtSubject || value || "Off Period",
+      course_name: time_table_data?.[row].course_name || "",
     };
 
     checkDuplicateFac();
   };
 
   useEffect(() => {
-    if(from_draft) {
-      checkDuplicateFac()
+    if (from_draft) {
+      checkDuplicateFac();
     }
-  }, [])
+  }, []);
 
   return (
     <div
@@ -174,6 +176,7 @@ export default function CellTimeTable({
       </div>
       <div className="flex flex-col">
         <select
+          key={"subject_name"}
           onChange={(e) => handleSubChange(e.currentTarget.value)}
           name="subject_name"
           defaultValue={virtual_table?.[`${row}:${col}`].subject || value}
@@ -193,9 +196,14 @@ export default function CellTimeTable({
         </select>
         <>
           <select
+            key={"fac_id"}
             onChange={(e) => handleFacChange(parseInt(e.currentTarget.value))}
             name="fac_id"
-            defaultValue={singleFacInfo?.faculty_id || "No Faculties"}
+            value={
+              singleFacInfo?.faculty_id
+                ? singleFacInfo.faculty_id
+                : "No Faculties"
+            }
             className="px-2 py-1 outline-none cursor-pointer bg-transparent"
           >
             {currentFaculties?.map((fac) => (
